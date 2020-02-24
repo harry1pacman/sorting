57740ba0d7a2b0a1bf142c6f756b029a12c8ac26
harry1pacman <55867944+harry1pacman@users.noreply.github.com>
2020-02-24 18:36:26 +0500

    Add files via upload

---
+++ selection_sort.py.py
@@ -0,0 +1,9 @@
+text = input()
+print(text)
+A = input("Введите системное время ПК: ")
+
+for i in range(len(text)):
+    if text[i] == ".":
+        text = (text.replace(text[i], A))
+
+print(text)
